3cfd32356f1f8dd50004bd8770647f5cc6c92021
eampadu1 <eampadu02@gmail.com>
2016-10-18 22:23:32 -0400

    Update process.php

--- public_html/php/process.php
+++ public_html/php/process.php
@@ -1,13 +1,11 @@
 <DOCTYPE !html>
 <html lang="en">
 <body style="background-color:cyan; color:"black; text-align:center; font-family: "Courier New",Arial;">
-<?php
-$NAME= $_POST["name"];
-$EMAIL=$_POST["email"];
-$TELEPHONE=$_POST["telephone"];
 
+<?php
 echo "<p>Welcome <strong>".$_POST["name"]."</strong>";
 echo "<p>I will contact you at: <bold>".$_POST["email"]."</bold><br>Telephone Number:".$_POST["telephone"]."<br><br>Bye";
 ?>
+                                                                                                       
 </body>
 </html>
