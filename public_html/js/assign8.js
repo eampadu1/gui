a0d0e321964a48549c0e25881f2794ae5376f1d7
eampadu1 <eampadu02@gmail.com>
2016-12-06 11:08:52 -0500

    Added Validation Statements to Slider

--- public_html/js/assign8.js
+++ public_html/js/assign8.js
@@ -1,51 +1,28 @@
 /*
 **Author: Ebenezer Ampadu
-**Program: assign6.js
+**Program: assign8.js
 **Description: JavaScript Program for Assignment 6
 */
 
 
 /*Function generateTable: builds multiplication table*/
 function generateTable() {
-
 var mTable = null;
 mTable = document.getElementById("multTable");
 
-var col_start = rfirst;
-alert(typeof(col_start));
-var col_end = rend;
-var row_start = cfirst;
-var row_end = cend;
+var col_start = Number(rfirst);
+var col_end = Number(rend);
+var row_start = Number(cfirst);
+var row_end = Number(cend);
 
-/*Input Validation: 1 - 50*/
-if ((parseInt(col_start) < 1) || (parseInt(col_start) > 50)) {
-document.getElementById("err").innerHTML = "Error: Check Horizontal Start Value";
-return;
-}
-
-alert (" first checkpoint");
-if ((parseInt(col_end) < 1) || (parseInt(col_end) > 50)) {
-document.getElementById("err").innerHTML = "Error: Check Horizontal End Value";
-return;
-}
-
-if ((parseInt(row_start) < 1) || (parseInt(row_start) > 50)) {
-document.getElementById("err").innerHTML = "Error: Check Vertical Start Value";
-return;
-}
-
-if ((parseInt(row_end) < 1) || (parseInt(row_end) > 50)) {
-document.getElementById("err").innerHTML = "Error: Check Vertical End Value";
-return;
-}
-
+/*Input Validation*/
 if (parseInt(col_start) >= parseInt(col_end)) {
-document.getElementById("err").innerHTML = "Error: Horizontal Start >= Horizontal End";
+document.getElementById("err").innerHTML = "Error: Horizontal Start Entry is Greater than Horizontal End Entry";
 return;
 }
 
 if (parseInt(row_start) >= parseInt(row_end)) {
-document.getElementById("err").innerHTML = "Error: Vertical Start >= Vertical End";
+document.getElementById("err").innerHTML = "Error: Vertical Start Entry is Greater than Vertical End Entry";
 return;
 }
 
