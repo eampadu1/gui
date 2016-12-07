70f161492d66f0f372d481a808ea1344256d10a8
eampadu1 <eampadu02@gmail.com>
2016-12-06 23:39:50 -0500

    Assignment 9

---
+++ public_html/js/assign9.js
@@ -0,0 +1,15 @@
+/*
+** Author: Ebenezer Ampadu
+** Program: assign9.js
+** Description: JavaScript for Assignment 9
+*/
+
+
+var Deal = function() {
+var first = Math.floor((Math.random() * 27 + 1);
+var second = Math.floor((Math.random() * 27 + 1);
+var third = Math.floor((Math.random() * 27 + 1);
+var fourth = Math.floor((Math.random() * 27 + 1);
+
+return;
+}
