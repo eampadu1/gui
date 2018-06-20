e5ce259a4fdf75a78a1e9ba4e8436e8236e64b4a
eampadu1 <eampadu02@gmail.com>
2018-06-20 02:19:41 -0400

    Update javascript.js

--- public_html/js/javascript.js
+++ public_html/js/javascript.js
@@ -11,9 +11,10 @@ document.getElementById("year").innerHTML = datum.getFullYear();
 
 var main_form = ()=>{
 alert("Thank you for submitting your form. Have a nice day!");
-};
 document.getElementById("name").innerHTML = " ";
 document.getElementById("email").innerHTML = " ";
 document.getElementById("telephone").innerHTML = " "; 
+};
+
 
 
